f0b07af6d15dc4bb049026220ff3a0e950c4f8f5
Pedal0 <perax33@gmail.com>
2024-10-24 16:12:52 +0200

    Refactor attack logic and add win condition
    Simplified the attack logic in the main function to use a while loop and added a win condition to determine the winner. Also added unit tests for player attacks in test_main.py.

--- main.py
+++ main.py
@@ -9,8 +9,24 @@ class Character:
 def main():
     player1 = Character("Player 1")
     player2 = Character("Player 2")
-    player1.attack(player2)
-    print(player2.hp)
+    
+    while player1.hp > 0 and player2.hp > 0:
+          
+          player1.attack(player2)
+
+          if player2.hp <= 0:
+            print('Player 1 has won')
+            break
+          print(f'Player 2 HP: {player2.hp}')
+
+          player2.attack(player1)
+
+          if player1.hp <= 0:
+            print('Player 2 has won')
+            break
+          print(f'Player 1 HP: {player1.hp}')
+ 
+
 
 if __name__ == "__main__":
     main()
--- test_main.py
+++ test_main.py
@@ -12,13 +12,25 @@ class TestCharacter(unittest.TestCase):
         self.assertEqual(player1.hp, 10)
         self.assertEqual(player2.hp, 10)
         print("Test init passed 2 players with 10 HP each")
-
-    def test_attaque(self):
+    
+    def test_attaque_player1(self):
         player1 = Character("Player 1")
         player2 = Character("Player 2")
-        player1.attack(player2)
-        self.assertEqual(player2.hp, 9)
-        print("Test attaque passed player 2 has 9 HP after attack")
+        for i in range(player1.hp):
+            player1.attack(player2)
+            self.assertEqual(player2.hp, player1.hp - i - 1)
+            print(f"Test attaque passed player 2 has {player1.hp - i - 1} HP after attack")
    
+    def test_attaque_player2(self):
+        player1 = Character("Player 1")
+        player2 = Character("Player 2")
+        for i in range(player2.hp):
+            player2.attack(player1)
+            self.assertEqual(player1.hp, player2.hp - i - 1)
+            print(f"Test attaque passed player 1 has {player2.hp - i - 1} HP after attack")
+
+
+
+
 if __name__ == "__main__":
     unittest.main()
