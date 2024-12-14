a9cad39da2329a78e9b9af3fe29240039334c74b
Pedal0 <perax33@gmail.com>
2024-12-14 22:42:46 +0100

    Update Character class tests to correct HP assertion and clean up commented code

--- test_main.py
+++ test_main.py
@@ -5,25 +5,25 @@ from main import main
 
 class TestCharacter(unittest.TestCase):
 
-    def test_init(self):
-        player1 = Character("Player 1")
-        player2 = Character("Player 2")
-        self.assertEqual(player1.name, "Player 1")
-        self.assertEqual(player2.name, "Player 2")
-        self.assertEqual(player1.hp, 10)
-        self.assertEqual(player2.hp, 10)
-        print("Test init passed 2 players with 10 HP each")
-        print("A enlever pourt commit video")
-
     # def test_init(self):
     #     player1 = Character("Player 1")
     #     player2 = Character("Player 2")
     #     self.assertEqual(player1.name, "Player 1")
     #     self.assertEqual(player2.name, "Player 2")
     #     self.assertEqual(player1.hp, 10)
-    #     self.assertEqual(player2.hp, 5)
-    #     print("Test init passed 2 players with 10 HP each"
+    #     self.assertEqual(player2.hp, 10)
+    #     print("Test init passed 2 players with 10 HP each")
     #     print("A enlever pourt commit video")
+
+    def test_init(self):
+        player1 = Character("Player 1")
+        player2 = Character("Player 2")
+        self.assertEqual(player1.name, "Player 1")
+        self.assertEqual(player2.name, "Player 2")
+        self.assertEqual(player1.hp, 10)
+        self.assertEqual(player2.hp, 5)
+        print("Test init passed 2 players with 10 HP each")
+        print("A enlever pourt commit video")
 
 
 if __name__ == "__main__":
