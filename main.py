8ac4c184b5866f5b3da255d4f738094eae8af1a5
Pedal0 <perax33@gmail.com>
2024-12-14 22:31:19 +0100

    Refactor main function to remove unnecessary encapsulation and improve readability

--- main.py
+++ main.py
@@ -1,26 +1,23 @@
-import Character
+from Character import Character
 
      
-def main():
-    player1 = Character("Player 1")
-    player2 = Character("Player 2")
+
+player1 = Character("Player 1")
+player2 = Character("Player 2")
     
-    while player1.hp > 0 and player2.hp > 0:
-          
-          player1.attack(player2)
+while player1.hp > 0 and player2.hp > 0:
+      
+      player1.attack(player2)
 
-          if player2.hp <= 0:
-            print('Player 1 has won')
-            break
-          print(f'Player 2 HP: {player2.hp}')
+      if player2.hp <= 0:
+        print('Player 1 has won')
+        break
+      print(f'Player 2 HP: {player2.hp}')
 
-          player2.attack(player1)
+      player2.attack(player1)
 
-          if player1.hp <= 0:
-            print('Player 2 has won')
-            break
-          print(f'Player 1 HP: {player1.hp}')
- 
+      if player1.hp <= 0:
+        print('Player 2 has won')
+        break
+      print(f'Player 1 HP: {player1.hp}')
 
-if __name__ == "__main__":
-    main()
